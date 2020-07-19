1eb78a1e9dc2cf7e630426e268163dc96280d179
hamdi619 <66076748+hamdi619@users.noreply.github.com>
2020-07-19 20:03:17 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<!-- <meta charset=" utf-8"> -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<title>INSEA inscription</title>
+    <link rel="icon" href="images/icon.png">
+  	<!-- Tell the browser to be responsive to screen width -->
+  	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  	<!-- Bootstrap 3.3.7 -->
+  	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  	<!-- DataTables -->
+    <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  	<!-- Font Awesome -->
+  	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  	<!-- Theme style -->
+  	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  	<!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  	<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+
+
+
+  	<!-- Google Font -->
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+
+
+  	<!-- Custom CSS -->
+    <style type="text/css">
+    /* Small devices (tablets, 768px and up) */
+    @media (min-width: 768px){ 
+      #navbar-search-input{ 
+        width: 60px; 
+      }
+      #navbar-search-input:focus{ 
+        width: 100px; 
+      }
+    }
+
+    /* Medium devices (desktops, 992px and up) */
+    @media (min-width: 992px){ 
+      #navbar-search-input{ 
+        width: 150px; 
+      }
+      #navbar-search-input:focus{ 
+        width: 250px; 
+      } 
+    }
+
+    .word-wrap{
+      overflow-wrap: break-word;
+    }
+    .prod-body{
+      height:300px;
+    }
+
+    .box:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+    .register-box{
+      margin-top:40px;
+      width: 600px
+    }
+
+    #trending{
+      list-style: none;
+      padding:10px 5px 10px 15px;
+    }
+    #trending li {
+      padding-left: 1.3em;
+    }
+    #trending li:before {
+      content: "\f046";
+      font-family: FontAwesome;
+      display: inline-block;
+      margin-left: -1.3em; 
+      width: 1.3em;
+    }
+
+    select {
+      height: 34px;
+    }
+
+    .center-img {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      max-width: 800px;
+      max-height: : 800px;
+    }
+    </style>
+
+
+
+</head>
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-blue layout-top-nav">
